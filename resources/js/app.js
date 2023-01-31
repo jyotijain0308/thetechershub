0390288a03a774e4dc79240223c97085616bad9e
Jyoti Jain <jjain@fooddudesdelivery.com>
2023-01-31 20:24:19 +0400

    Launching Page Design Done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,11 @@
 import './bootstrap';
 
 import '../sass/app.scss'
+
+import '../css/bootstrap-icons/bootstrap-icons.css'
+
+import '../css/style.css'
+
+import '../js/main.js'
+
+import '../js/php-email-form/validate.js'
