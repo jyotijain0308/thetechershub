934c57f7754561389ced49f49526fb9acfefbfd6
Ruchika Jain <ruchika.jain94@gmail.com>
2023-02-03 14:18:37 +0530

    Message: Related to your task measning message

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,28 @@ import '../css/bootstrap-icons/bootstrap-icons.css'
 
 import '../css/style.css'
 
+import '../css/home-style.css'
+
+import '../css/aos.css'
+
+import '../css/boxicons.min.css'
+
+import '../css/glightbox.min.css'
+
+import '../css/remixicon.css'
+
+import '../css/swiper-bundle.min.css'
+
 import '../js/main.js'
 
 import '../js/php-email-form/validate.js'
+
+import '../js/aos.js'
+
+import '../js/glightbox.min.js'
+
+import '../js/isotope.pkgd.min.js'
+
+import '../js/swiper-bundle.min.js'
+
+
