f80a0968d580b71fa04ecdb7367bb1c31c2c7116
Ruchika Jain <ruchika.jain94@gmail.com>
2023-03-19 16:52:51 +0530

    config file images and videos renamed

--- config/portfolio.php
+++ config/portfolio.php
@@ -4,14 +4,80 @@ return [
 		[
 			"title" => 'Logo 1',
 			"type" => 'image',
-			"url" => '/img/portfolio/images/Aaroyan Foundation.jpg',
+			"url" => '/img/portfolio/images/Aaroyan-Foundation.jpg',
 			"alt" => 'Aaroyan Foundation'
 		],
 		[
 			"title" => 'Video 1',
 			"type" => 'video',
-			"url" => '/img/portfolio/videos/Video-27.mp4',
-			"alt" => 'Aaroyan Foundation'
-		]
+			"url" => '/img/portfolio/videos/FAB-Texture.mp4',
+			"alt" => 'FAB-Texture'
+		],
+		[
+			"title" => 'Logo 2',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Bino-Brokers.png',
+			"alt" => 'Bino Brokers'
+		],
+		[
+			"title" => 'Logo 3',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Dhanji-Logo-Board.png',
+			"alt" => 'Dhanji Logo Board'
+		],
+		[
+			"title" => 'Logo 4',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Flavia.png',
+			"alt" => 'Flavia'
+		],
+		[
+			"title" => 'Video 2',
+			"type" => 'video',
+			"url" => '/img/portfolio/videos/JIJA-JI-Mobile-App.mp4',
+			"alt" => 'JIJA JI Mobile App'
+		],
+		[
+			"title" => 'Video 3',
+			"type" => 'video',
+			"url" => '/img/portfolio/videos/Anantara-Resort-Jaisalmer.mp4',
+			"alt" => 'Anantara Resort Jaisalmer'
+		],
+		[
+			"title" => 'Logo 5',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Hari-Glass-Store.png',
+			"alt" => 'Hari Glass Store'
+		],
+		[
+			"title" => 'Logo 6',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Local-Findy.jpg',
+			"alt" => 'Local Findy'
+		],
+		[
+			"title" => 'Logo 7',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Marwar-Buildcon.png',
+			"alt" => 'Marwar Buildcon'
+		],
+		[
+			"title" => 'Video 5',
+			"type" => 'video',
+			"url" => '/img/portfolio/videos/Marwar-Buildcon.mp4',
+			"alt" => 'Marwar-Buildcon'
+		],
+		[
+			"title" => 'Logo 8',
+			"type" => 'image',
+			"url" => '/img/portfolio/images/Vally-Interior.jpg',
+			"alt" => 'Vally Interior'
+		],
+		[
+			"title" => 'Video 6',
+			"type" => 'video',
+			"url" => '/img/portfolio/videos/Quality-Service.mp4',
+			"alt" => 'Quality-Service'
+		],
 	]
 ];
