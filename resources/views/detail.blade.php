d290f9ca1ce89ca7db0af1a850eb631c4e5a61c4
Ruchika Jain <ruchika.jain94@gmail.com>
2023-03-20 22:38:45 +0530

    images added in portfolio section

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('css')
+@endsection
+@section('content')
+
+<main id="main">
+	<section id="" class="">
+		<div class="container">
+			Detail page
+		</div>
+	</section>
+</main><!-- End #main -->
+@endsection
+
+@section('js')
+<!-- Vendor JS Files -->
+<script src="/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="/vendor/aos/aos.js"></script>
+<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/js/php-email-form/validate.js"></script>
+<script src="/js/main.js"></script>
+@endsection
